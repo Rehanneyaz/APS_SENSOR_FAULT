cfd4f278dcdc3b61e19c1e9278946c1702ba9df9
Rehan Ahmed <rehan.lcm@gmail.com>
2024-05-24 00:32:36 +0530

    artifact folder generated

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
 import sys,os
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
 
-def test_logger_and_exception():
+
+
+
+if __name__== "__main__":
     try:
-        logging.info("starting the test_logger_exception")
-        result = 3/0
-        print(result)
-        logging.info("stoping the test_logger_exception")
-    except Exception as e:
-        logging.debug(str(e))
-        raise SensorException(e,sys)
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+    except Exception  as e:
+        print(e)
+        
     
-    
-if __name__=="__main__":
-    try:
-        test_logger_and_exception()
-    except Exception as e:
-        print(e)  
