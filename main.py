d0120549b05f58462f9adf0a60187221445d9b6a
Rehan Ahmed <rehan.lcm@gmail.com>
2024-05-24 04:00:49 +0530

    file data updted

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
-
+from sensor.utils import get_collection_as_dataframe
 
 if __name__== "__main__":
     try:
@@ -32,6 +32,8 @@ if __name__== "__main__":
         model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
+        
+        
         
     except Exception  as e:
         print(e)
