674bf011f655dc8617baa25bf1a7c1664f93168d
Rehan Ahmed <rehan.lcm@gmail.com>
2024-05-24 19:44:45 +0530

    model evaluation and pusher task done

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
-
+from sensor.components.model_evaluation import ModelEvaluation
+from sensor.components.model_pusher import ModelPusher
 
 if __name__== "__main__":
     try:
+        # data ingestion
         training_pipeline_config = config_entity.TrainingPipelineConfig()
         data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         print(data_ingestion_config.to_dict())
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         
-        
+        # data validation
         data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
         data_validation = DataValidation(data_validation_config=data_validation_config,
         data_ingestion_artifact=data_ingestion_artifact)
@@ -34,6 +36,20 @@ if __name__== "__main__":
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
         
+        #model evaluation
+        model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+        model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
+        data_ingestion_artifact=data_ingestion_artifact,
+        data_transformation_artifact=data_transformation_artifact,
+        model_trainer_artifact=model_trainer_artifact)
+        model_eval_artifact = model_eval.initiate_model_evaluation()
+
+        #model pusher
+        model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config)
+        model_pusher = ModelPusher(model_pusher_config=model_pusher_config, 
+        data_transformation_artifact=data_transformation_artifact,
+        model_trainer_artifact=model_trainer_artifact)
+        model_pusher_artifact = model_pusher.initiate_model_pusher()
     
         
         
